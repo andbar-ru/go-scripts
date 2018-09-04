c58ca5010495371504b8df08971eb4d4a82266fa
Andrei Bartashevich <andrei.bartashevich@krontech.ru>
2018-09-04 18:49:54 +0300

    Parallelize AverageColor

--- average_color/average_color.go
+++ average_color/average_color.go
@@ -4,32 +4,60 @@ import (
 	"image"
 	"image/color"
 	"math"
+	"sync"
 
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
 )
 
+type colors struct {
+	mux    sync.Mutex
+	reds   uint64
+	greens uint64
+	blues  uint64
+	alphas uint64
+}
+
+func (c *colors) Inc(r, g, b, a uint64) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+
+	c.reds += r
+	c.greens += g
+	c.blues += b
+	c.alphas += a
+}
+
 func AverageColor(img image.Image) color.NRGBA {
 	bounds := img.Bounds()
 	pxNumber := bounds.Max.X * bounds.Max.Y
-	var reds, greens, blues, alphas uint64
+	clrs := &colors{reds: 0, greens: 0, blues: 0, alphas: 0}
+
+	var wg sync.WaitGroup
+	wg.Add(bounds.Max.X)
 	for x := 0; x < bounds.Max.X; x++ {
-		for y := 0; y < bounds.Max.Y; y++ {
-			red, green, blue, alpha := img.At(x, y).RGBA()
-			reds += uint64(red)
-			greens += uint64(green)
-			blues += uint64(blue)
-			alphas += uint64(alpha)
-		}
+		go func(x int) {
+			defer wg.Done()
+			var lReds, lGreens, lBlues, lAlphas uint64
+			for y := 0; y < bounds.Max.Y; y++ {
+				red, green, blue, alpha := img.At(x, y).RGBA()
+				lReds += uint64(red)
+				lGreens += uint64(green)
+				lBlues += uint64(blue)
+				lAlphas += uint64(alpha)
+			}
+			clrs.Inc(lReds, lGreens, lBlues, lAlphas)
+		}(x)
 	}
+	wg.Wait()
 
 	var red, green, blue, alpha uint8
 	k := uint64(0xffff / 0xff)
-	red = uint8(math.Round(float64(reds/k) / float64(pxNumber)))
-	green = uint8(math.Round(float64(greens/k) / float64(pxNumber)))
-	blue = uint8(math.Round(float64(blues/k) / float64(pxNumber)))
-	alpha = uint8(math.Round(float64(alphas/k) / float64(pxNumber)))
+	red = uint8(math.Round(float64(clrs.reds/k) / float64(pxNumber)))
+	green = uint8(math.Round(float64(clrs.greens/k) / float64(pxNumber)))
+	blue = uint8(math.Round(float64(clrs.blues/k) / float64(pxNumber)))
+	alpha = uint8(math.Round(float64(clrs.alphas/k) / float64(pxNumber)))
 
 	return color.NRGBA{red, green, blue, alpha}
 }

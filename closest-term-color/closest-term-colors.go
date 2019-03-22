bdddc8ebbeb54e09a7237709e47d666a55574f57
Andrei Bartashevich <andrei.bartashevich@krontech.ru>
2019-03-22 16:53:03 +0300

    Support multiple colors to output

--- closest-term-color/closest-term-colors.go
+++ closest-term-color/closest-term-colors.go
@@ -13,6 +13,7 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"sort"
 	"strconv"
 )
 
@@ -24,7 +25,7 @@ var (
 	colors                []color
 	colorRegex            = regexp.MustCompile(`(?i)^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$`)
 	numberRegex           = regexp.MustCompile(`^\d+$`)
-	srcRgb                *rgb
+	srcRgb                rgb
 	numberOfClosestColors = 1
 )
 
@@ -41,6 +42,11 @@ type color struct {
 	Name string `json:"name"`
 }
 
+type colorDistance struct {
+	color    color
+	distance float64
+}
+
 func check(err error) {
 	if err != nil {
 		log.Panic(err)
@@ -50,6 +56,7 @@ func check(err error) {
 func printHelpAndExit() {
 	fmt.Printf("Usage: %s <color> [<number of closest colors to print>]\n", filepath.Base(os.Args[0]))
 	fmt.Println("color must be given in hex format, e.g. '#131723'")
+	fmt.Printf("number of closest colors is optional (1 by default), must be integer and less than number of available colors (%d).\n", len(colors))
 	os.Exit(1)
 }
 
@@ -67,18 +74,7 @@ func fillColors() {
 	}
 }
 
-func (rgb1 *rgb) getDistance(rgb2 rgb) float64 {
-	redDiff := float64(rgb1.Red) - float64(rgb2.Red)
-	greenDiff := float64(rgb1.Green) - float64(rgb2.Green)
-	blueDiff := float64(rgb1.Blue) - float64(rgb2.Blue)
-	return math.Sqrt(redDiff*redDiff + greenDiff*greenDiff + blueDiff*blueDiff)
-}
-
-func init() {
-	fillColors()
-}
-
-func main() {
+func validateArgs() {
 	args := os.Args[1:]
 	if len(args) < 1 || len(args) > 2 {
 		printHelpAndExit()
@@ -96,7 +92,8 @@ func main() {
 	check(err)
 	blue, err := strconv.ParseUint(submatches[3], 16, 8)
 	check(err)
-	srcRgb = &rgb{Red: uint8(red), Green: uint8(green), Blue: uint8(blue)}
+
+	srcRgb = rgb{Red: uint8(red), Green: uint8(green), Blue: uint8(blue)}
 
 	if len(args) == 2 {
 		numberArg := args[1]
@@ -106,18 +103,37 @@ func main() {
 		}
 		numberStr := numberRegex.FindStringSubmatch(numberArg)[0]
 		numberOfClosestColors, err = strconv.Atoi(numberStr)
+		if numberOfClosestColors > len(colors) {
+			fmt.Printf("ERROR: number is greater than number of available colors: %d > %d", numberOfClosestColors, len(colors))
+			printHelpAndExit()
+		}
 		check(err)
 	}
+}
 
-	minDistance := 500.0
-	var closestColor color
+func getDistance(rgb1, rgb2 rgb) float64 {
+	redDiff := float64(rgb1.Red) - float64(rgb2.Red)
+	greenDiff := float64(rgb1.Green) - float64(rgb2.Green)
+	blueDiff := float64(rgb1.Blue) - float64(rgb2.Blue)
+	return math.Sqrt(redDiff*redDiff + greenDiff*greenDiff + blueDiff*blueDiff)
+}
+
+func init() {
+	fillColors()
+}
+
+func main() {
+	validateArgs()
+
+	colorDistances := make([]colorDistance, 0, len(colors))
 	for _, color := range colors {
-		distance := srcRgb.getDistance(color.RGB)
-		if distance < minDistance {
-			minDistance = distance
-			closestColor = color
-		}
+		colorDistances = append(colorDistances, colorDistance{color, getDistance(srcRgb, color.RGB)})
 	}
+	sort.Slice(colorDistances, func(i, j int) bool {
+		return colorDistances[i].distance < colorDistances[j].distance
+	})
 
-	fmt.Printf("Closest color is %d (%s: %s) (distance %.2f)\n", closestColor.Id, closestColor.Hex, closestColor.Name, minDistance)
+	for _, cd := range colorDistances[:numberOfClosestColors] {
+		fmt.Printf("%d (%s: %s) (distance %.2f)\n", cd.color.Id, cd.color.Hex, cd.color.Name, cd.distance)
+	}
 }

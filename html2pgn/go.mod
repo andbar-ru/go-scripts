723aae90e7189a859f019ab610c67c2b3c7c62b6
Andrei Bartashevich <andrei.bartashevich@krontech.ru>
2021-02-27 22:35:02 +0300

    html2pgn

---
+++ html2pgn/go.mod
@@ -0,0 +1,5 @@
+module html2pgn
+
+go 1.15
+
+require golang.org/x/net v0.0.0-20210224082022-3d97a244fca7 // indirect
---
+++ html2pgn/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+const (
+	headingClass         = "Heading_20_3"
+	movesClass           = "Standard"
+	predefinedMovesClass = "T1"
+	noveltyClass         = "T2"
+)
+
+var (
+	inHeading         = false
+	inMoves           = false
+	inPredefinedMoves = false
+	inNovelty         = false
+	round             int
+	currentResult     string
+)
+
+var (
+	commonTags = `[Event "Stockfish vs Stockfish games"]
+[Site "localhost"]
+[Date "????.??.??"]
+[White "Stockfish 12"]
+[Black "Stockfish 12"]
+[TimeControl "1800+0"]
+`
+	// Round, Result, ECO tags are dymamic.
+
+	pgn string
+)
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getClass(token html.Token) string {
+	for _, attr := range token.Attr {
+		if attr.Key == "class" {
+			return attr.Val
+		}
+	}
+	return ""
+}
+
+func fillTags(heading string) {
+	pgn += commonTags
+	fields := strings.Fields(heading)
+	if len(fields) < 3 {
+		return
+	}
+	round++
+	pgn += fmt.Sprintf("[Round \"%d\"]\n", round)
+	pgn += fmt.Sprintf("[ECO \"%s\"]\n", fields[1])
+	result := fields[2]
+	result = strings.Replace(result, ":", "-", 1)
+	result = strings.ReplaceAll(result, "½", "1/2")
+	pgn += fmt.Sprintf("[Result \"%s\"]\n", result)
+	currentResult = result
+	pgn += "\n"
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("you must give file path as the first argument")
+	}
+	filePath := os.Args[1]
+	file, err := os.Open(filePath)
+	checkErr(err)
+	tokenizer := html.NewTokenizer(file)
+L:
+	for {
+		tokenType := tokenizer.Next()
+		switch tokenType {
+		case html.ErrorToken:
+			break L
+		case html.StartTagToken:
+			switch class := getClass(tokenizer.Token()); class {
+			case headingClass:
+				inHeading = true
+			case movesClass:
+				inMoves = true
+			case predefinedMovesClass:
+				inPredefinedMoves = true
+			case noveltyClass:
+				inNovelty = true
+			}
+		case html.EndTagToken:
+			tagNameRaw, _ := tokenizer.TagName()
+			tagName := string(tagNameRaw)
+			switch tagName {
+			case "p":
+				switch {
+				case inHeading:
+					inHeading = false
+				case inMoves:
+					inMoves = false
+					pgn += " " + currentResult + "\n\n"
+				}
+			case "span":
+				if inPredefinedMoves {
+					inPredefinedMoves = false
+					pgn += " {end of predefined moves}"
+				}
+				if inNovelty {
+					inNovelty = false
+					pgn += " {novelty}"
+				}
+			}
+		case html.TextToken:
+			switch {
+			case inHeading:
+				heading := string(tokenizer.Text())
+				fillTags(heading)
+			case inMoves:
+				pgn += string(tokenizer.Text())
+			}
+		}
+	}
+
+	fmt.Println(pgn)
+}
